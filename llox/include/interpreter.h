57421b256f2e374aa4b681fbe1ca6f3d2b06bb5e
Kun Xi <kunxi@kunxi.org>
2026-08-10 16:12:07 -0700

    clang-formated.

--- llox/include/interpreter.h
+++ llox/include/interpreter.h
@@ -2,13 +2,13 @@
 #define INTERPRETER_H
 
 #include "syntax.h"
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
-#include "llvm/ExecutionEngine/ExecutionEngine.h"
-#include <memory>
 #include <map>
+#include <memory>
 
 using Value = llvm::Value;
 
@@ -17,37 +17,37 @@ public:
   Interpreter()
       : context(std::make_unique<llvm::LLVMContext>()),
         module(std::make_unique<llvm::Module>("llox jit", *context)),
-        builder(std::make_unique<llvm::IRBuilder<>>(*context)),
-        variables() {
+        builder(std::make_unique<llvm::IRBuilder<>>(*context)), variables() {
     // ponytail: create a dummy function so the builder has somewhere to insert
-    auto* ft = llvm::FunctionType::get(llvm::Type::getVoidTy(*context), false);
-    auto* fn = llvm::Function::Create(ft, llvm::Function::ExternalLinkage, "__llox_main", *module);
-    auto* bb = llvm::BasicBlock::Create(*context, "entry", fn);
+    auto *ft = llvm::FunctionType::get(llvm::Type::getVoidTy(*context), false);
+    auto *fn = llvm::Function::Create(ft, llvm::Function::ExternalLinkage,
+                                      "__llox_main", *module);
+    auto *bb = llvm::BasicBlock::Create(*context, "entry", fn);
     builder->SetInsertPoint(bb);
   };
 
   // StmtVisitor
-  void visit(const ExprStmt& stmt) override;
-  void visit(const PrintStmt& stmt) override;
-  void visit(const VarStmt& stmt) override;
+  void visit(const ExprStmt &stmt) override;
+  void visit(const PrintStmt &stmt) override;
+  void visit(const VarStmt &stmt) override;
 
-  void execute(const std::vector<std::unique_ptr<Stmt>>& program);
+  void execute(const std::vector<std::unique_ptr<Stmt>> &program);
 
   // CodegenVisitor
-  Value* visit(const Literal& expr) override;
-  Value* visit(const Unary& expr) override;
-  Value* visit(const Binary& expr) override;
-  Value* visit(const Grouping& expr) override;
+  Value *visit(const Literal &expr) override;
+  Value *visit(const Unary &expr) override;
+  Value *visit(const Binary &expr) override;
+  Value *visit(const Grouping &expr) override;
 
 private:
   llvm::FunctionCallee get_printf();
-  Value* log_error_v(const char* s);
+  Value *log_error_v(const char *s);
 
   std::unique_ptr<llvm::LLVMContext> context;
   std::unique_ptr<llvm::Module> module;
   std::unique_ptr<llvm::IRBuilder<>> builder;
   std::unique_ptr<llvm::ExecutionEngine> engine;
-  std::map<std::string, Value*> variables;
+  std::map<std::string, Value *> variables;
 };
 
 #endif
